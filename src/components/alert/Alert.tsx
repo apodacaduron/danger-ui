7befe3ff3f9b3d58313b3357769eb0897ade3eb8
Daniel Apodaca <apodacaduron@gmail.com>
2021-01-16 14:54:53 -0700

    fix(alert): code and sass cleanup

--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -1,67 +1,40 @@
-import React from 'react'
+import classNames from 'classnames'
+import React, { FC } from 'react'
+import { AlertProps } from 'utils/interfaces'
 
 import './Alert.sass'
 
-interface IProps {
-  children: JSX.Element
-  position?: string
-  color?: string
-  visible?: boolean
-  onClick: () => void
-}
-
-const Alert = ({
+const Alert: FC<AlertProps> = ({
   children,
   color = 'primary',
   position = 'top-center',
   visible = false,
   onClick
-}: IProps) => {
-  const getBackgroundColor = () => {
-    switch (color) {
-      case 'primary':
-        return 'DangerPrimaryBackground'
-      case 'danger':
-        return 'DangerDangerBackground'
-      case 'warning':
-        return 'DangerWarningBackground'
-      case 'success':
-        return 'DangerSuccessBackground'
-      default:
-        return 'DangerPrimaryBackground'
-    }
-  }
-
-  const getPosition = () => {
-    switch (position) {
-      case 'top-left':
-        return 'DangerPositionTopLeft'
-      case 'top-center':
-        return 'DangerPositionTopCenter'
-      case 'top-right':
-        return 'DangerPositionTopRight'
-      case 'bottom-left':
-        return 'DangerPositionBottomLeft'
-      case 'bottom-center':
-        return 'DangerPositionBottomCenter'
-      case 'bottom-right':
-        return 'DangerPositionBottomRight'
-      default:
-        return 'DangerPositionTopCenter'
-    }
-  }
+}) => {
+  const containerClasses = classNames({
+    'danger-alert-container': true,
+    'danger-hide': !visible,
+    'danger-position-top-left': position === 'top-left',
+    'danger-position-top-center': position === 'top-center',
+    'danger-position-top-right': position === 'top-right',
+    'danger-position-bottom-left': position === 'bottom-left',
+    'danger-position-bottom-center': position === 'bottom-center',
+    'danger-position-bottom-right': position === 'bottom-right',
+    'danger-primary-background': color === 'primary',
+    'danger-success-background': color === 'success',
+    'danger-warning-background': color === 'warning',
+    'danger-danger-background': color === 'danger'
+  })
 
   return (
     <div
       onClick={onClick}
-      className={`${'DangerAlertContainer'} ${getBackgroundColor()} ${getPosition()} ${
-        !visible ? 'DangerHide' : ''
-      }`}
+      className={containerClasses}
       style={{
         background: color !== 'primary' ? color : ''
       }}
     >
-      <div className={'DangerAlertText'}>{children}</div>
+      <div className='danger-alert-text'>{children}</div>
     </div>
   )
 }
