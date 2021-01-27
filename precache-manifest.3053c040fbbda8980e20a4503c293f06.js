d737e4f99d97ed5abfedfe58b3148549cc0bf564
Daniel Apodaca <apodacaduron@gmail.com>
2021-01-27 15:46:03 -0700

    Updates

---
+++ precache-manifest.3053c040fbbda8980e20a4503c293f06.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92e004b4e55331adb1884e3f2f07db2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "acde9a1f4c7501a79cd5",
+    "url": "./static/css/2.38195e76.chunk.css"
+  },
+  {
+    "revision": "8de3181c554ef3d78d1c",
+    "url": "./static/css/main.1396b425.chunk.css"
+  },
+  {
+    "revision": "acde9a1f4c7501a79cd5",
+    "url": "./static/js/2.7461fee2.chunk.js"
+  },
+  {
+    "revision": "183d810e37bd1f933fd347e0d9fb24ae",
+    "url": "./static/js/2.7461fee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de3181c554ef3d78d1c",
+    "url": "./static/js/main.7b1fc95f.chunk.js"
+  },
+  {
+    "revision": "6ff446d126308f2d54be",
+    "url": "./static/js/runtime-main.2df6a33f.js"
+  },
+  {
+    "revision": "e0a8489e672d7759feaf05bf042be4a5",
+    "url": "./static/media/background.e0a8489e.jpg"
+  },
+  {
+    "revision": "60a3ef701bf0c58d762e8001309379f5",
+    "url": "./static/media/danger-logo.60a3ef70.svg"
+  }
+]);
